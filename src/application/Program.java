c241ccc720cc55351d6f64254dd20ecefd552daa
Roberta de Siqueira <designer.roberta@gmail.com>
2022-01-18 21:27:00 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	/**
@@ -7,7 +9,8 @@ public class Program {
 	 */
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world!");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
