9af5d90fbb0f4b1bf8274e7914b27daabe7dbdc1
Roberta de Siqueira <designer.roberta@gmail.com>
2022-02-16 20:28:58 -0300

    organizing java files

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -3,7 +3,7 @@ package chess;
 import boardgame.Position;
 
 public class ChessPosition {
-
+	
 	private char column;
 	private int row;
 	
@@ -14,21 +14,21 @@ public class ChessPosition {
 		this.column = column;
 		this.row = row;
 	}
-	
+
 	public char getColumn() {
 		return column;
 	}
-	
+
 	public int getRow() {
 		return row;
 	}
-	
-	protected Position toPosition(){
+
+	protected Position toPosition() {
 		return new Position(8 - row, column - 'a');
 	}
 	
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+		return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
 	}
 	
 	@Override
